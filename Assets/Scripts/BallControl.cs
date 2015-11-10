4bfb26df05da693a609a50498f5b49c79bfcedc4
José Medeiros <nicruo@mbp-de-jose.agap2.corp>
2015-11-10 13:29:49 +0000

    First commit

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControl : MonoBehaviour {
+
+	public bool InControl = true;
+
+	private Rigidbody rigidBody;
+
+	public float MovementForce = 10;
+	
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+
+		if (InControl) {
+
+			var horizontal = Input.GetAxis("Horizontal");
+			var vertical = Input.GetAxis("Vertical");
+
+			rigidBody.AddForce(horizontal * MovementForce, 0 , vertical * MovementForce);
+
+			if (Input.GetKey (KeyCode.R)) {
+				Reset();
+			}
+		}
+
+		if (transform.position.y <= -10)
+			Reset ();
+	}
+
+	void Reset()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		InControl = false;
+	}
+}
---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallSpawner : MonoBehaviour {
+
+	public GameObject[] balls;
+
+	public Text ballCountText;
+
+	public float minSize = 5;
+	public float maxSize = 20;
+
+	private int ballCount = 0;
+
+	private GameObject currentBall;
+	
+	void Start () {
+		SpawnBall ();
+	}
+
+	private void SpawnBall()
+	{
+		var randomBall = balls[Random.Range(0,balls.Length)];
+		currentBall = Instantiate (randomBall, transform.position, transform.rotation) as GameObject;
+		var randomSize = Random.Range (minSize, maxSize);
+		currentBall.transform.localScale = new Vector3 (randomSize, randomSize, randomSize);
+		ballCount++;
+	}
+
+	void Update () {
+		if (!currentBall.GetComponent<BallControl> ().InControl) {
+			SpawnBall();
+		}
+
+		ballCountText.text = ballCount + " Balls";
+	}
+}
